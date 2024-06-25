47f3c9924abc81a9477b1b193cf869945b173be7
Beominator <woqja87@gmail.com>
2024-06-25 16:54:06 +0900

    0625

---
+++ JAVA_Basic/src/java0625/StudyJava.java
@@ -0,0 +1,12 @@
+package java0625;
+
+public class StudyJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("자바테스트");		// 자바에서 출력하는 방법 중 하나
+		System.out.println();
+		
+	}
+
+}
