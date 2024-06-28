4262b919cef7e3dcada4899c4c108299343e3f25
Beominator <woqja87@gmail.com>
2024-06-28 09:56:18 +0900

    0628

--- JAVA_Basic/src/java0627/Loop.java
+++ JAVA_Basic/src/java0627/Loop.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Loop {
 
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		
 		
 		// 반복문 - for, while, do~while
 		//for(초기값; 조건식; 증감식;) { 반복내용 }
@@ -46,7 +47,31 @@ public class Loop {
 		System.out.print("총합 : " + sum);
 		
 		
+//		for(; num1!=0; ) {
+//			System.out.println("정수 입력 : ");
+//			int num = scan.nextInt();
+//			if(num == 0) break;
+//		}
 		
+		// 무한루프 종료시키는법
+		// 1.if문의 조건식이 참이되면 break 실행
+		// 2 for( ; 조건식 ;)
+		// 3. return; = 반복문코드가 있는 메서드릴 종료시키기때문에 반복문도 종료된다.
+		// break; - 반복문, switch 사용가능
+		//			if문 단독으로 break 사용불가
+		// if(10>5) break; -> 오류!
+		
+		//continue
+		
+		for (int i=1; i<=10; i++) {
+			System.out.println(i+"번째 반복 시작");
+			if(i%3 == 0) continue;
+			System.out.println(i+"번째 반복 진행중");
+			System.out.println(i+"번째 반복 마지막 내용");
+		}
+		
+		// 자바의 랜덤
+		int random = (int)Math.floor(Math.random()*10)+1;
 	}
 
 }
